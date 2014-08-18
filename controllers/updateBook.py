60f52a59a7607409118d18471960161e1753022a
wnxhaja <wnxhaja@gmail.com>
2014-08-18 22:01:31 +0800

    Create Test for updateBook    Add test for addAvailableCopies
       new method for updateBook
    	getAvailableCopies
    
    Changes to commit:
    	modified:   controllers/updateBook.py
    	modified:   tests/unittest/testUpdateBook.py

--- controllers/updateBook.py
+++ controllers/updateBook.py
@@ -1,10 +1,16 @@
 
 __author__ = 'librarymanagementteam'
 
-# recieve ajax request with json key isbn and no_of_copies
-def addAvaialbleCopies():
+def getAvailableCopies(isbn):
+	no_of_copies = db(db.book.ISBN == isbn).select(db.book.available_copies)
+	return no_of_copies[0].available_copies
+
+#recieve ajax request with json key isbn	
+def addAvailableCopies():
 	isbn = request.vars.isbn
-	no_of_copies = request.vars.no_of_copies
-	db(db.book.isbn == isbn).update(available_copies = no_of_copies + 1)
+	no_of_copies = getAvailableCopies(isbn)
+	db(db.book.ISBN == isbn).update(available_copies = no_of_copies + 1)
+
 
 def reduceAvailableCopies():
+	pass
--- tests/unittest/testUpdateBook.py
+++ tests/unittest/testUpdateBook.py
@@ -5,8 +5,21 @@ import json
 
 from gluon.globals import Request
 
+execfile("applications/librarymanagement/controllers/updateBook.py", globals())
 
+class TestUpdateBook(unittest.TestCase):
+	def setUp(self):
+		request = Request("")	
 
+	def testAddAvailableCopies(self):
+		db.commit()
+		isbn = '0-07-013151-2'
+		request.vars.isbn = isbn
+		expected = getAvailableCopies(isbn) + 1
+		addAvailableCopies()
+		result = getAvailableCopies(isbn)
+		db.rollback()
+		self.assertEquals(expected, result)
 
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestUpdateBook))
