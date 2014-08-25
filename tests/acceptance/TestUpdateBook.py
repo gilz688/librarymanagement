6c921bf7ece4789d9da1a219befc4b8722f45bbc
Gilmore Capiral <eromlig.ac@gmail.com>
2014-08-25 15:51:48 +0800

    updated testBorrowBook, testReturnBook in TestUpdateBook.py

--- tests/acceptance/TestUpdateBook.py
+++ tests/acceptance/TestUpdateBook.py
@@ -10,23 +10,66 @@ class TestUpdateBook(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        time.sleep(5)
+        time.sleep(3)
         cls.browser.quit()
     
-    def testBorrowBook(self):
-        # Visit URL 
+    def getAvailableCopies(self):
+        time.sleep(2)
+        return int(self.browser.find_by_xpath('//span[@id=\"availcopies\"]').first.value)
+
+    def getStatus(self):
+        time.sleep(2)
+        return self.browser.find_by_xpath('//span[@id=\"status\"]').first.value
+
+    def visitBooksUrl(self):
         url = "http://127.0.0.1:8000/librarymanagement/default/" 
         self.browser.visit(url) 
+
+    def testBorrowBook(self):
+        self.visitBooksUrl()
 
         # User clicks the "Introduction to Algorithm"
         book = self.browser.find_by_xpath('//td[text()=\"Introduction to Algorithms\"]').first
         book.click()
 
+        expected = self.getAvailableCopies() - 1
+
         # User clicks Borrow button
         borrowButton = self.browser.find_by_xpath('//button[text()=\"Borrow\"]').first
-        time.sleep(1)
+        
+        borrowButton.click()
+
+        self.browser.get_alert().accept()
+        alert = self.browser.get_alert()
+        if alert != None:
+            alert.accept()
+
+        actual = self.getAvailableCopies()
+        
+        self.assertEqual(expected,actual)
+
+    def testReturnBook(self):
+        self.visitBooksUrl()
+
+        # User clicks the "Introduction to Algorithm"
+        book = self.browser.find_by_xpath('//td[text()=\"Introduction to Algorithms\"]').first
+        book.click()
+
+        
+        expected = self.getAvailableCopies() + 1
+
+        # User clicks Borrow button
+        borrowButton = self.browser.find_by_xpath('//button[text()=\"Return\"]').first
+        
         borrowButton.click()
         self.browser.get_alert().accept()
+        alert = self.browser.get_alert()
+        if alert != None:
+            alert.accept()
+
+        actual = self.getAvailableCopies()
+        
+        self.assertEqual(expected,actual)
 
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestUpdateBook))
