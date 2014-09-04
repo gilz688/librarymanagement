28a65783084a6b717b87a85e870dd627ab511020
wnxhaja <wnxhaja@gmail.com>
2014-09-04 23:14:21 +0800

    Need to commit   Rename testSearchBookByTitleKeyword to tesSearchBookByTitleWithAKeyword

--- tests/unittest/testSearchBook.py
+++ tests/unittest/testSearchBook.py
@@ -19,7 +19,7 @@ class TestSearchBook(unittest.TestCase):
 		
 		db.rollback()
 
-	def testSearchByTitleKeyword(self):
+	def testSearchByTitleGivenAKeyword(self):
 		request.args = ['to']
 		expected = '[{"publisher": null, "ISBN": "0-07-013151-3", "description": null, "title": "to be or not to be", "pic": null, "available_copies": 11, "lib_name": "COE-Library", "no_of_copies": 11}, {"publisher": "MIT Press", "ISBN": "0-07-013151-1", "description": "This book covers a broad range of algorithms in depth, yet makes their design and analysis accessible to all levels of readers. Each chapter is relatively self-contained and can be used as a unit of study. The algorithms are described in English and in a pseudocode designed to be readable by anyone who has done a little programming. ", "title": "Introduction to Algorithms", "pic": "book.pic.8f51fb150a423756.362d3034366a6630352e6a7067.jpg", "available_copies": 0, "lib_name": "COE-Library", "no_of_copies": 20}]'
 		result = searchBookByTitle()
