ca614b89305e0bec56b79284128d3871c7642167
jal23 <agantal.jalal@gmail.com>
2014-09-22 02:12:08 +0800

    partial js for sign-in feature

--- static/js/libman/session.js
+++ static/js/libman/session.js
@@ -1,5 +1,39 @@
-function signInUser(){}
+function signInUser(){
+
+}
 
 function signOutUser(){
 	$( "#session a" ).html("Log In");
+}
+
+
+function validate(username,password){
+	if(checIfnotBlank(username,password)){
+		$.ajax({
+			type: "post",
+			url: local_site+ "auth/login",
+			data: {
+				username: username,
+				password: password
+			},
+			dataType: "json",
+			success: function(result){
+				//some codes here..
+			},
+		});
+	}
+	else{
+		//displays error in modal
+		$("#sign-in-error").html("Username and Password field is required.");
+	}
+}
+
+function checIfnotBlank(username,password){
+	var username = username.trim(),
+		password = password.trim();
+
+	if((username != "")&&(password != ""))
+		return true;
+	else
+		return false;
 }
