151654909cd979d5ae1fe5ac8a10d996f740fb7a
Gilmore Capiral <eromlig.ac@gmail.com>
2014-09-24 22:21:00 +0800

    added unit tests for logout, validate, and getLibrarian

--- tests/unittest/testAuth.py
+++ tests/unittest/testAuth.py
@@ -12,10 +12,16 @@ class TestAuth(unittest.TestCase):
 		request = Request("")
 
 	def testValidate(self):
-		pass
+		username = 'librarian1'
+		password = 'password1'
+		result = validate(username, password)
+		self.assertTrue(result)
 
 	def testValidateWithWrongCredentials(self):
-		pass
+		username = 'librarian1'
+		password = 'wrongpass'
+		result = validate(username, password)
+		self.assertFalse(result)
 
 	def testLogin(self):
 		try:
@@ -66,10 +72,36 @@ class TestAuth(unittest.TestCase):
 		self.assertFalse(isLoggedIn())
 
 	def testLogout(self):
-		pass
+		request.post_vars['username'] = 'librarian1'
+		request.post_vars['password'] = 'password1'
+
+		login()
+		self.assertTrue(isLoggedIn())
+
+		logout()
+		self.assertFalse(isLoggedIn())
 
 	def testLogoutWithUserNotLoggedIn(self):
-		pass
+		logout()
+		self.assertFalse(isLoggedIn())
+
+		logout()
+		self.assertFalse(isLoggedIn())
+
+	def testGetLibrarian(self):
+		request.post_vars['username'] = 'librarian1'
+		request.post_vars['password'] = 'password1'
+		login()
+		result = getLibrarian()
+		expected = {'librarian_id': '1999-0001', 'lib_name': 'COE-Library', 'username': 'librarian1', 'password': '$pbkdf2-sha256$200000$BwCgdM75X6u19p4TAiDkXA$JKHzME6MeIzbUP270RhyIle8L83Q7VNgIMMj3QGxQE', 'lname': 'Wiggins', 'fname': 'Adrew'}
+		self.assertEquals(result, expected)
+
+	def testGetLibrarianWithNoUserLoggedIn(self):
+		logout()
+		result = getLibrarian()
+		expected = None
+		self.assertEquals(result, expected)
+
 
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestAuth))
