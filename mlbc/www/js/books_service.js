21d77d738856c85f6e119c9f848adff3e8f3e5d9
Gilmore Capiral <eromlig.ac@gmail.com>
2014-10-06 23:49:48 +0800

    designed and coded initial UI for mobile app (index.html, book_details.html, settings.html) coded view books feature (initial code for books_controller.js books_service.js, settings_controller.js)

---
+++ mlbc/www/js/books_service.js
@@ -0,0 +1,65 @@
+function booksService($http, $q) {
+
+    // Return public API.
+    return ({
+        getSpecificBookInfo: getSpecificBookInfo,
+        getBookAuthors: getBookAuthors,
+        getBooks: getBooks
+    });
+
+    // ---
+    // PUBLIC METHODS.
+    // ---
+    function getSpecificBookInfo(ISBN) {
+        var request = $http({
+            method: "post",
+            url: remote_site + "viewBooks/getSpecificBookInfo",
+            data: {
+                isbn: ISBN
+            },
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+        return (request.then(handleSuccess, handleError));
+    }
+
+    function getBooks(library) {
+        var request = $http({
+            method: "get",
+            url: remote_site + "viewBooks/getBooks/" + library,
+        });
+        return (request.then(handleSuccess, handleError));
+    }
+
+    function getBookAuthors(ISBN) {
+        var request = $http({
+            method: "post",
+            url: remote_site + "viewBooks/getBookAuthors",
+            data: {
+                isbn: ISBN
+            },
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+        return (request.then(handleSuccess, handleError));
+    }
+
+    // ---
+    // PRIVATE METHODS.
+    // ---
+    function handleError(response) {
+        if (!angular.isObject(response.data) ||
+            !response.data.message
+        ) {
+            return ($q.reject("An unknown error occurred."));
+        }
+        return ($q.reject(response.data.message));
+    }
+
+    function handleSuccess(response) {
+        console.dir(response.data);
+        return (response.data);
+    }
+}
