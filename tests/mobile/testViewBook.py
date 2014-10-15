e89dba9108112db2ee3a60b55d70a70bc4c42b21
Gilmore Capiral <eromlig.ac@gmail.com>
2014-10-16 00:48:08 +0800

    modified appium app path quit session when done

--- tests/mobile/testViewBook.py
+++ tests/mobile/testViewBook.py
@@ -5,18 +5,20 @@ from appium import webdriver
 class TestViewBook(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-    	desired_caps = {}
-    	desired_caps['platformName'] = 'Android'
-    	desired_caps['deviceName'] = 'Android Emulator'
-    	desired_caps['app'] = os.path.abspath('./applications/librarymanagement/mlbc/platforms/android/ant-build/LibraryBookChecker-debug.apk')
-    	self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        desired_caps = {}
+        desired_caps['platformName'] = 'Android'
+        desired_caps['deviceName'] = 'Android Emulator'
+        desired_caps['app'] =  os.path.abspath(os.path.join(os.path.dirname(__file__),'../../mlbc/platforms/android/ant-build/LibraryBookChecker-debug.apk'))
+        print desired_caps['app']
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
     @classmethod
     def tearDownClass(self):
-    	return None
-        
+        self.driver.quit()
+
     def testViewBook(self):
         pass
+
 
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestViewBook))
