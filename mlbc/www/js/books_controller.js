fe184295484a1f0c69ea84c8a4159d0264ea877b
Gilmore Capiral <eromlig.ac@gmail.com>
2014-10-20 11:10:17 +0800

    partial code for searchBook infinite pagination added start and end variables in searchAllBook (searchBook.py) tweaked Books Controller for mobile app

--- mlbc/www/js/books_controller.js
+++ mlbc/www/js/books_controller.js
@@ -1,40 +1,50 @@
 function booksController($scope, $q, booksService) {
+    $scope.ready = true;
     viewBooks();
 
-        // bind function to html element with ng-click="viewSpecificBookInfo"
+    // bind function to html element with ng-click="viewSpecificBookInfo"
     $scope.viewSpecificBookInfo = viewSpecificBookDetails;
     $scope.searchBook = searchAllBook;
     $scope.baseUrl = remote_site;
     $scope.viewBooks = viewBooks;
 
-    function viewBooks(){
+    function viewBooks() {
         $scope.books = [];
-        addBooks(1,3);
+        addBooks(1, 3);
+        bindViewBooksToNextPage();
     }
 
-    $scope.nextPage = function(){
-        addBooks($scope.books.length,$scope.books.length+1);
+    function bindViewBooksToNextPage(){
+        $scope.nextPage = function() {
+            addBooks($scope.books.length, $scope.books.length + 1);
+        }
     }
 
-    function addBooks(start,end) {
-        booksService.getAllBooks(start,end).then(
-            function(books) {
-                console.dir(books);
-                for(var i=0;i<books.length;i++){
-                    $scope.books.push(books[i]);
+    function addBooks(start, end) {
+        if ($scope.ready){
+            $scope.ready = false;
+            booksService.getAllBooks(start, end).then(
+                function(books) {
+
+                    console.dir(books);
+                    for (var i = 0; i < books.length; i++) {
+                        $scope.books.push(books[i]);
+                    }
+
+                    $scope.ready = true;
                 }
-            }
-        );
+            );
+        }
     }
 
     function setBooks(aBooks) {
         $scope.books = aBooks;
     }
 
-    function viewSpecificBookDetails(isbn){
-    	booksService.getSpecificBookInfo(isbn).then(
+    function viewSpecificBookDetails(isbn) {
+        booksService.getSpecificBookInfo(isbn).then(
             function(book) {
-            	$scope.currentBook = book;
+                $scope.currentBook = book;
                 var options = {
                     animation: 'slide', // What animation to use
                     onTransitionEnd: function() {} // Called when finishing transition animation
@@ -45,14 +55,13 @@ function booksController($scope, $q, booksService) {
     }
 
 
-    function searchAllBook(keyword){
+    function searchAllBook(keyword) {
         $scope.books = [];
-        if (keyword == ""){
+        if (keyword == "") {
             viewBooks();
-        }
-        else{
+        } else {
             booksService.searchBook(keyword).then(
-                function(books){
+                function(books) {
                     setBooks(books);
                 }
             );
